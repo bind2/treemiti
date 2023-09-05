aa81610eeb9aedf6435908a997c514ab35b00fae
deepak <deepak7890bind@gmail.com>
2023-09-05 22:46:38 +0530

    add hamburger menu

--- src/components/course-features/CourseFeatures.js
+++ src/components/course-features/CourseFeatures.js
@@ -5,12 +5,12 @@ import { NavLink } from 'react-router-dom'
 const CourseFeatures = () => {
   return (
     <ul className='features-list'>
-        <li><NavLink to='/treemiti/CareerGuidance'><img src="" alt="" /><h3>Career Guidance</h3></NavLink></li>
-        <li><NavLink to='/treemiti/IndustrialSyllabus'><img src="" alt="" /><h3>Industrial Syllabus</h3></NavLink></li>
-        <li><NavLink to='/treemiti/ExperiencedFaculties'><img src="" alt="" /><h3>Experienced Faculties</h3></NavLink></li>
-        <li><NavLink to='/treemiti/TrustedCertification'><img src="" alt="" /><h3>Trusted Certification</h3></NavLink></li>
-        <li><NavLink to='/treemiti/PersonalDevelopment'><img src="" alt="" /><h3>Personal Development</h3></NavLink></li>
-        <li><NavLink to='/treemiti/PlacementAssistance'><img src="" alt="" /><h3>Placement Assistance</h3></NavLink></li>
+        <li><NavLink to='/treemiti/CareerGuidance'><img src="" alt="" /><h3>Career <br/> Guidance</h3></NavLink></li>
+        <li><NavLink to='/treemiti/IndustrialSyllabus'><img src="" alt="" /><h3>Industrial <br/> Syllabus</h3></NavLink></li>
+        <li><NavLink to='/treemiti/ExperiencedFaculties'><img src="" alt="" /><h3>Experienced <br/> Faculties</h3></NavLink></li>
+        <li><NavLink to='/treemiti/TrustedCertification'><img src="" alt="" /><h3>Trusted <br/> Certification</h3></NavLink></li>
+        <li><NavLink to='/treemiti/PersonalDevelopment'><img src="" alt="" /><h3>Personal <br/> Development</h3></NavLink></li>
+        <li><NavLink to='/treemiti/PlacementAssistance'><img src="" alt="" /><h3>Placement <br/> Assistance</h3></NavLink></li>
         
     </ul>
   )
