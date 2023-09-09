cb8b57fcc8d4936452031463f9f5ee5a3b3ad41b
deepak <deepak7890bind@gmail.com>
2023-09-09 19:42:52 +0530

    navbar changed

--- src/layout/navbar/Navbar.js
+++ src/layout/navbar/Navbar.js
@@ -1,38 +1,35 @@
-import React, { useEffect } from 'react'
+import React, { useState } from 'react'
 // import './Navbar.scss'
 import { HashLink as Link } from 'react-router-hash-link'
 import logo from '../../logo-img/treemiti-white-logo.png'
 const Navbar = () => {
-  useEffect(() => {
-    const menuBar = document.querySelector(".menu-bar")
-    const menuIcon = document.querySelector(".menu-icon")
-    const navLinks = document.querySelector(".nav-links")
-    menuBar.addEventListener('click', () => {
-      menuBar.classList.toggle('menu-bar-active')
-      menuIcon.classList.toggle('menu-active')
-      navLinks.classList.toggle('nav-links-active')
-    })
-  })
+  const [state, setState] = useState({menu: true})
+
+  const showHide = ()=>{
+    setState({menu: !state.menu})
+  }
+
+
   return (
     <nav>
       <div className="container" id='navbar'>
         <Link to='/treemiti/#'><img src={logo} alt="logo" /></Link>
 
-        <ul className='nav-links'>
-          <li><Link to='/treemiti/#'>Home</Link></li>
-          <li><Link to='/treemiti/#courses'>Courses</Link></li>
-          <li><Link to='/treemiti/#about'>About us</Link></li>
-          <li><Link to='/treemiti/#testimonials'>Testimonials</Link></li>
-          <li><Link to='/treemiti/#placements'>Placements</Link></li>
-          <li><Link to='/treemiti/#guidance'>Guidance</Link></li>
-          <li><Link to='/treemiti/#contact'>Contact</Link></li>
+        <ul className={state.menu ? 'nav-links' : 'nav-links-active'}>
+          <li onClick={showHide}><Link to='/treemiti/#'>Home</Link></li>
+          <li onClick={showHide}><Link to='/treemiti/#courses'>Courses</Link></li>
+          <li onClick={showHide}><Link to='/treemiti/#about'>About us</Link></li>
+          <li onClick={showHide}><Link to='/treemiti/#testimonials'>Testimonials</Link></li>
+          <li onClick={showHide}><Link to='/treemiti/#placements'>Placements</Link></li>
+          <li onClick={showHide}><Link to='/treemiti/#guidance'>Guidance</Link></li>
+          <li onClick={showHide}><Link to='/treemiti/#contact'>Contact</Link></li>
         </ul>
 
         {/* <div>qr</div> */}
 
-        <div className="menu-bar">
+        <div className={state.menu ? "menu-bar" : "menu-bar menu-bar-active"} onClick={showHide}>
           <span className='menu-name'>menu</span>
-          <span className='menu-icon'></span>
+          <span className={state.menu ? 'menu-icon' : 'menu-icon menu-active'}></span>
         </div>
 
       </div>
